eaf5183ca130ac1e6aca3095caf3099a76061bee
Guilherme da Rocha <109252470+GRochaC@users.noreply.github.com>
2023-10-16 18:02:41 -0300

    Update FatoracaoTeste.java

--- infinite_mirror/src/FatoracaoTeste.java
+++ infinite_mirror/src/FatoracaoTeste.java
@@ -22,4 +22,40 @@ public class FatoracaoTeste {
 
         assertEquals(map1, fat.get_fatoracao(2));
     }
+
+    @Test
+    public void fatoracao461() {
+        Map<Integer,Integer> map1 = new HashMap<>();
+        map1.put(461, 1);
+
+        assertEquals(map1, fat.get_fatoracao(461));
+    }
+
+    @Test
+    public void fatoracao728() {
+        Map<Integer,Integer> map1 = new HashMap<>();
+        map1.put(2, 3);
+        map1.put(7, 1);
+        map1.put(13, 1);
+
+        assertEquals(map1, fat.get_fatoracao(728));
+    }
+
+    @Test
+    public void fatoracao1000() {
+        Map<Integer,Integer> map1 = new HashMap<>();
+        map1.put(2, 3);
+        map1.put(5, 3);
+
+        assertEquals(map1, fat.get_fatoracao(1000));
+    }
+
+    @Test
+    public void fatoracao76() {
+        Map<Integer,Integer> map1 = new HashMap<>();
+        map1.put(2, 2);
+        map1.put(19, 1);
+
+        assertEquals(map1, fat.get_fatoracao(76));
+    }
 }
